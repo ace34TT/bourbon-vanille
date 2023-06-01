c2539616cb02c83cc0d2353df12ad49663dc7c80
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-06-01 10:02:24 +0300

    setup : tailwindcss

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div>
+      <div className="px-44 flex justify-around items-center h-screen w-screen bg-gray-500">
+        <div className="flex-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis illum
+          nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+          inventore autem quibusdam voluptatibus, placeat rem nisi enim. Omnis,
+          expedita totam.
+        </div>
+        <div className="flex-1"></div>
+      </div>
+      <div className="px-44 flex justify-around items-center h-screen w-screen bg-yellow-500">
+        <div className="flex-1"></div>
+        <div className="flex-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis illum
+          nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+          inventore autem quibusdam voluptatibus, placeat rem nisi enim. Omnis,
+          expedita totam.
+        </div>
+      </div>
+      <div className="px-44 flex justify-around items-center h-screen w-screen bg-green-500">
+        <div className="flex-1"></div>
+        <div className="flex-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis illum
+          nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+          inventore autem quibusdam voluptatibus, placeat rem nisi enim. Omnis,
+          expedita totam.
+        </div>
+      </div>
+      <div className="px-44 flex justify-around items-center h-screen w-screen bg-red-500">
+        <div className="flex-1"></div>
+        <div className="flex-1">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis illum
+          nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+          inventore autem quibusdam voluptatibus, placeat rem nisi enim. Omnis,
+          expedita totam.
+        </div>
+      </div>
+    </div>
+  );
+}
