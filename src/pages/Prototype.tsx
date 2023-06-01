fa927a5adbca850bf3e7f034fe34b196d333c828
Rabenandrasana Tafinasoa <tafinasoa35@gmail.com>
2023-06-01 23:51:18 +0300

    feat : background change

--- src/pages/Prototype.tsx
+++ src/pages/Prototype.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-gsap.registerPlugin(ScrollTrigger);
 //
 const loadImage = async (path: string): Promise<string> => {
   try {
@@ -20,10 +19,15 @@ const imagePaths = [
   "./../assets/backgrounds/3.jpg",
   "./../assets/backgrounds/5.jpg",
 ];
-
+const sectionRelations = [
+  { bg: "bg-4", subSection: "sub-section-2" },
+  { bg: "bg-3", subSection: "sub-section-3" },
+  { bg: "bg-2", subSection: "sub-section-4" },
+];
 export default function Prototype() {
   const [loadedImages, setLoadedImages] = useState<any[]>([]);
-
+  const containerRef = useRef<any>(null);
+  gsap.registerPlugin(ScrollTrigger);
   useEffect(() => {
     const loadImages = async () => {
       const loaded = await Promise.all(
@@ -33,97 +37,153 @@ export default function Prototype() {
     };
     loadImages();
   }, []);
-  useEffect(() => {}, []);
+
+  // useEffect(() => {
+  //   const animateElement = () => {
+  //     gsap.fromTo(".bg-4", { opacity: 1 }, { opacity: 0, duration: 0.5 });
+  //   };
+  //   const reverseAnimation = () => {
+  //     gsap.fromTo(".bg-4", { opacity: 0 }, { opacity: 1, duration: 0.5 });
+  //   };
+  //   ScrollTrigger.create({
+  //     markers: true,
+  //     trigger: ".sub-section-2",
+  //     start: "top 80%",
+  //     end: "bottom 15%",
+  //     scrub: true,
+  //     onEnter: animateElement,
+  //     onLeaveBack: reverseAnimation,
+  //   });
+  //   const animateElement2 = () => {
+  //     gsap.fromTo(".bg-3", { opacity: 1 }, { opacity: 0, duration: 0.5 });
+  //   };
+  //   const reverseAnimation2 = () => {
+  //     gsap.fromTo(".bg-3", { opacity: 0 }, { opacity: 1, duration: 0.5 });
+  //   };
+  //   ScrollTrigger.create({
+  //     markers: true,
+  //     trigger: ".sub-section-3",
+  //     start: "top 80%",
+  //     end: "bottom 15%",
+  //     scrub: true,
+  //     onEnter: animateElement2,
+  //     onLeaveBack: reverseAnimation2,
+  //   });
+  // }, []);
+  useEffect(() => {
+    const animateElement = (bg: string) => {
+      gsap.fromTo(
+        "." + bg,
+        { opacity: 1, y: -50 },
+        { opacity: 0, y: 0, duration: 0.5 }
+      );
+    };
+    const reverseAnimation = (bg: string) => {
+      gsap.fromTo("." + bg, { opacity: 0 }, { opacity: 1, duration: 0.5 });
+    };
+    const createScrollTrigger = (element: {
+      bg: string;
+      subSection: string;
+    }) => {
+      ScrollTrigger.create({
+        markers: true,
+        trigger: "." + element.subSection,
+        start: "top 80%",
+        end: "bottom 15%",
+        onEnter: () => animateElement(element.bg),
+        onLeaveBack: () => reverseAnimation(element.bg),
+      });
+    };
+
+    sectionRelations.forEach((element) => createScrollTrigger(element));
+  }, []);
+
   return (
     <>
-      {loadedImages.length === imagePaths.length && (
-        <>
-          <div className="3d-section relative">
-            <div className="w-screen h-screen main-section sticky top-0 relative">
-              <div
-                className="w-full h-full absolute z-10"
-                style={{ zIndex: 10 }}
-              >
-                <img
-                  src={loadedImages[0]}
-                  alt="Your Image"
-                  className="w-full h-full object-cover"
-                ></img>
-              </div>
-              <div
-                className="w-full h-full absolute z-20"
-                style={{ zIndex: 20 }}
-              >
-                <img
-                  src={loadedImages[1]}
-                  alt="Your Image"
-                  className="w-full h-full object-cover"
-                ></img>
-              </div>
-              <div
-                className="w-full h-full absolute z-30"
-                style={{ zIndex: 30 }}
-              >
-                <img
-                  src={loadedImages[2]}
-                  alt="Your Image"
-                  className="w-full h-full object-cover"
-                ></img>
-              </div>
-              <div
-                className="w-full h-full absolute z-40"
-                style={{ zIndex: 40 }}
-              >
-                <img
-                  src={loadedImages[3]}
-                  alt="Your Image"
-                  className="w-full h-full object-cover"
-                ></img>
-              </div>
-            </div>
-            {/*  */}
-            <div className="sub-section-1 px-32 flex justify-around items-center h-screen w-screen absolute top-0  z-20 bg-gray-500 opacity-50">
-              <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui
-                fugiat inventore autem quibusdam voluptatibus, placeat rem nisi
-                enim. Omnis, expedita totam.
-              </div>
-              <div className="flex-1"></div>
-            </div>
-            <div className="sub-section-2 px-32 flex justify-around items-center h-screen w-screen z-20 bg-yellow-500 opacity-50">
-              <div className="flex-1"></div>
-              <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui
-                fugiat inventore autem quibusdam voluptatibus, placeat rem nisi
-                enim. Omnis, expedita totam.
-              </div>
-            </div>
-            <div className="sub-section-3 px-32 flex justify-around items-center h-screen w-screen z-20 bg-green-500 opacity-50">
-              <div className="flex-1"></div>
-              <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui
-                fugiat inventore autem quibusdam voluptatibus, placeat rem nisi
-                enim. Omnis, expedita totam.
-              </div>
-            </div>
-            <div className="sub-section-3 px-32 flex justify-around items-center h-screen w-screen z-20 bg-red-500 opacity-50">
-              <div className="flex-1"></div>
-              <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui
-                fugiat inventore autem quibusdam voluptatibus, placeat rem nisi
-                enim. Omnis, expedita totam.
-              </div>
-            </div>
+      <div ref={containerRef} className="3d-section relative">
+        <div className="w-screen h-screen main-section sticky top-0 relative">
+          <div
+            className="bg-1 w-full h-full absolute z-10 brightness-50"
+            style={{ zIndex: 10 }}
+          >
+            <img
+              src={loadedImages[0]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
           </div>
-          <div className="h-screen bg-gray-500 flex justify-center items-center">
-            Footer
+          <div
+            className="bg-2 w-full h-full absolute z-20 brightness-50"
+            style={{ zIndex: 20 }}
+          >
+            <img
+              src={loadedImages[1]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
           </div>
-        </>
-      )}
+          <div
+            className="bg-3 w-full h-full absolute z-30 brightness-50"
+            style={{ zIndex: 30 }}
+          >
+            <img
+              src={loadedImages[2]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
+          </div>
+          <div
+            className="bg-4 w-full h-full absolute z-40 brightness-50"
+            style={{ zIndex: 40 }}
+          >
+            <img
+              src={loadedImages[3]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
+          </div>
+        </div>
+        {/*  */}
+        <div className="sub-section-1 px-32 flex justify-around items-center h-screen w-screen absolute top-0 z-50 opacity-100">
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+          <div className="flex-1"></div>
+        </div>
+        <div className="sub-section-2 px-32 flex justify-around items-center h-screen w-screen z-50 opacity-75">
+          <div className="flex-1"></div>
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+        </div>
+        <div className="sub-section-3 px-32 flex justify-around items-center h-screen w-screen z-50 opacity-75">
+          <div className="flex-1"></div>
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+        </div>
+        <div className="sub-section-4 px-32 flex justify-around items-center h-screen w-screen z-50 opacity-75">
+          <div className="flex-1"></div>
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+        </div>
+      </div>
+      <div className="footer h-screen bg-gray-500 flex justify-center items-center">
+        Footer
+      </div>
     </>
   );
 }
