ef01e4e414eb4083459e475fd85ba59ef8879502
iFabruce <fabrucemaminiaina@gmail.com>
2023-10-19 10:10:52 +0300

    file

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,153 @@
-import Image from 'next/image'
+import { useEffect, useRef, useState } from "react";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
-export default function Home() {
+//
+const loadImage = async (path: string): Promise<string> => {
+  try {
+    const imageModule = await import(path);
+    return imageModule.default;
+  } catch (error) {
+    console.error("Error loading image:", error);
+    return "";
+  }
+};
+
+const imagePaths = [
+  "/images/1.jpg",
+  "/images/2.jpg",
+  "/images/3.jpg",
+  "/images/5.jpg",
+];
+const sectionRelations = [
+  { bg: "bg-4", subSection: "sub-section-2" },
+  { bg: "bg-3", subSection: "sub-section-3" },
+  { bg: "bg-2", subSection: "sub-section-4" },
+];
+export default function Prototype() {
+  const [loadedImages, setLoadedImages] = useState<any[]>([]);
+  const containerRef = useRef<any>(null);
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    const loadImages = async () => {
+      const loaded = await Promise.all(
+        imagePaths.map((path) => loadImage(path))
+      );
+      setLoadedImages(loaded);
+    };
+    loadImages();
+  }, []);
+
+  useEffect(() => {
+    const animateElement = (bg: string) => {
+      gsap.fromTo("." + bg, { opacity: 1 }, { opacity: 0, duration: 1 });
+    };
+    const reverseAnimation = (bg: string) => {
+      gsap.fromTo("." + bg, { opacity: 0 }, { opacity: 1, duration: 0.5 });
+    };
+    const createScrollTrigger = (element: {
+      bg: string;
+      subSection: string;
+    }) => {
+      ScrollTrigger.create({
+        markers: true,
+        trigger: "." + element.subSection,
+        start: "top 80%",
+        end: "bottom 15%",
+        onEnter: () => animateElement(element.bg),
+        onLeaveBack: () => reverseAnimation(element.bg),
+      });
+    };
+
+    sectionRelations.forEach((element) => createScrollTrigger(element));
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div ref={containerRef} className="3d-section relative">
+        <div className="w-screen h-screen main-section sticky top-0 relative">
+          <div
+            className="bg-1 w-full h-full absolute z-10 brightness-50"
+            style={{ zIndex: 10 }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <img
+              src={loadedImages[0]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
+          </div>
+          <div
+            className="bg-2 w-full h-full absolute z-20 brightness-50"
+            style={{ zIndex: 20 }}
+          >
+            <img
+              src={loadedImages[1]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
+          </div>
+          <div
+            className="bg-3 w-full h-full absolute z-30 brightness-50"
+            style={{ zIndex: 30 }}
+          >
+            <img
+              src={loadedImages[2]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
+          </div>
+          <div
+            className="bg-4 w-full h-full absolute z-40 brightness-50"
+            style={{ zIndex: 40 }}
+          >
+            <img
+              src={loadedImages[3]}
+              alt="Your Image"
+              className="w-full h-full object-cover"
+            ></img>
+          </div>
+        </div>
+        {/*  */}
+        <div className="sub-section-1 px-32 flex justify-around items-center h-screen w-screen absolute top-0 z-50 opacity-100">
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+          <div className="flex-1"></div>
+        </div>
+        <div className="sub-section-2 px-32 flex justify-around items-center h-screen w-screen z-50 opacity-75">
+          <div className="flex-1"></div>
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+        </div>
+        <div className="sub-section-3 px-32 flex justify-around items-center h-screen w-screen z-50 opacity-75">
+          <div className="flex-1"></div>
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
+        </div>
+        <div className="sub-section-4 px-32 flex justify-around items-center h-screen w-screen z-50 opacity-75">
+          <div className="flex-1"></div>
+          <div className="flex-1 prose max-w-none text-5xl font-extrabold text-white">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+            illum nihil deleniti repellat ipsam. Aliquid mollitia in, qui fugiat
+            inventore autem quibusdam voluptatibus, placeat rem nisi enim.
+            Omnis, expedita totam.
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="footer h-screen bg-gray-500 flex justify-center items-center">
+        Footer
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
